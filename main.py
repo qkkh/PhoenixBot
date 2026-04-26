c8c1dee5dd2ca0dde9d573722e5e69fa6857f67e
qkkh <4almohandi343@gmail.com>
2026-04-27 02:46:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class MyBot(commands.Bot):
                 avatar = Editor(avatar_image).resize((170, 170)).circle_image()
                 
                 # الإحداثيات الموزونة للوسط بالضبط
-                background.paste(avatar, (54, 74)) 
+                background.paste(avatar, (52, 72)) 
                 
                 file = discord.File(fp=background.image_bytes, filename="welcome_card.png")
                 await channel.send(content=welcome_text, file=file)
